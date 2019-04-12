dace75f248d382ee47b324c5069aaf4740a33d0a
mokgadirasekgala <rasekgalam@gmail.com>
2019-04-12 15:44:57 +0200

    add REQUIRED_FIELDS to the CustomerUser Model

--- users/models.py
+++ users/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class CustomUser(AbstractUser):
 
     life_motto = models.TextField()
+    
+    REQUIRED_FIELDS = ['life_motto']#to propt for on createsuperuser
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
