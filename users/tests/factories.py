4d5176d19ccd46c697ea35a383ab638a76890e5c
mokgadirasekgala <rasekgalam@gmail.com>
2019-04-13 09:39:20 +0200

    Update factories.py

--- users/tests/factories.py
+++ users/tests/factories.py
@@ -12,6 +12,7 @@ class CustomUserFactory(DjangoModelFactory):
     email = factory.Faker("email")
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
+    life_motto = factory.Faker("sentence")
 
     class Meta:
         model = CustomUser
